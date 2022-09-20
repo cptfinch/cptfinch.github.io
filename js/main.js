074ad5dd06d09b7c3526ae6e5d27f934a5e2c1a6
Christopher Finch <cptfinch@gmail.com>
2022-09-20 14:30:50 +0100

    Update

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var list=['blah', 'yah', 'boo'];
-var i=0;
-/* const toggle_text = function () {
-  document.getElementById('.cf_para1').innerHTML = list[i%3];
-  i++;
-} */
-document.getElementById('toggle_button').addEventListener("click", function(){document.getElementById('cf_para1').innerHTML = list[(i++)%3];}); //
